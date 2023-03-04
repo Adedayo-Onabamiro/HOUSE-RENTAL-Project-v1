6171fececf5a884ae2f6a25c3ba82c9f3be05a54
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-03-04 18:06:55 +0100

    Complete responsive code excluding only map

--- home.js
+++ home.js
@@ -24,4 +24,60 @@
         slides[slideIndex-1].style.display = "block";  
         dots[slideIndex-1].className += " active";
       }
+
+      // code for hamburger responsiveness
+      const hamburger = document.querySelector('.hamburger');
+      const mobile_menu = document.querySelector('nav .landUl');
+      const menu_item = document.querySelectorAll('nav .landUl .landLi a');
+      const header = document.querySelector('.pg-div');
+
+      hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+
+        if (hamburger.classList.contains("active")) {
+          document.body.style.overflow = "hidden";        
+            } else {
+              document.body.style.overflow = "auto";
+        }
+
+
+      });
       
+      document.addEventListener('scroll', () => {
+        var scroll_position = window.scrollY;
+        if (scroll_position > 250) {
+          header.style.backgroundColor = 'white';
+        } else {
+          header.style.backgroundColor = 'transparent';
+        }
+      });
+
+
+
+      menu_item.forEach((item) => {
+        item.addEventListener('click', () => {
+          hamburger.classList.toggle('active');
+          mobile_menu.classList.toggle('active');
+        });
+      });
+
+      function reveal() {
+        var reveal = document.querySelectorAll(".reveal");
+        
+        for (var i = 0; i < reveal.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveal[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+        
+          if (elementTop < windowHeight - elementVisible) {
+          reveal[i].classList.add("active");
+          } else {
+          reveal[i].classList.remove("active");
+          }
+        }
+        }
+        
+        window.addEventListener("scroll", reveal);
+        
+      // code for responsiveness ends
